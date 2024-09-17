44d051117500ec3534f9b5aae52aba98428b44a8
LakshanWC <lakshanwc@gmail.com>
2024-09-17 10:25:43 +0530

    All deliver team reColored

--- FinalProject/three-tier architecture/PL/DeliveryTeam/Delivery.Designer.cs
+++ FinalProject/three-tier architecture/PL/DeliveryTeam/Delivery.Designer.cs
@@ -69,7 +69,7 @@
             this.btn_special_Order_delivery.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
             this.btn_special_Order_delivery.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_special_Order_delivery.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_special_Order_delivery.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btn_special_Order_delivery.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(115)))), ((int)(((byte)(51)))));
             this.btn_special_Order_delivery.Location = new System.Drawing.Point(431, 307);
             this.btn_special_Order_delivery.Name = "btn_special_Order_delivery";
             this.btn_special_Order_delivery.Size = new System.Drawing.Size(145, 46);
@@ -94,7 +94,7 @@
             this.btn_normal_orders.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
             this.btn_normal_orders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_normal_orders.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_normal_orders.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btn_normal_orders.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(115)))), ((int)(((byte)(51)))));
             this.btn_normal_orders.Location = new System.Drawing.Point(582, 307);
             this.btn_normal_orders.Name = "btn_normal_orders";
             this.btn_normal_orders.Size = new System.Drawing.Size(145, 46);
@@ -108,7 +108,7 @@
             this.btn_accsepted_orders.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
             this.btn_accsepted_orders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_accsepted_orders.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_accsepted_orders.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btn_accsepted_orders.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(115)))), ((int)(((byte)(51)))));
             this.btn_accsepted_orders.Location = new System.Drawing.Point(582, 363);
             this.btn_accsepted_orders.Name = "btn_accsepted_orders";
             this.btn_accsepted_orders.Size = new System.Drawing.Size(145, 46);
@@ -122,7 +122,7 @@
             this.btn_update_status.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
             this.btn_update_status.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_update_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_update_status.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btn_update_status.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(167)))), ((int)(((byte)(69)))));
             this.btn_update_status.Location = new System.Drawing.Point(311, 372);
             this.btn_update_status.Name = "btn_update_status";
             this.btn_update_status.Size = new System.Drawing.Size(105, 29);
@@ -157,7 +157,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(115)))), ((int)(((byte)(51)))));
             this.label3.Location = new System.Drawing.Point(22, 384);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(84, 13);
@@ -167,7 +167,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(115)))), ((int)(((byte)(51)))));
             this.label1.Location = new System.Drawing.Point(22, 337);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 13);
@@ -201,7 +201,7 @@
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.label14.ForeColor = System.Drawing.Color.Yellow;
             this.label14.Location = new System.Drawing.Point(12, 9);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(62, 15);
